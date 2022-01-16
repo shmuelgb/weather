cf841c7689ab957235268e62ebd220e2ee9232a2
shmulgb <93119163+shmuelgb@users.noreply.github.com>
2022-01-16 16:41:55 +0200

    cors

--- app.js
+++ app.js
@@ -6,48 +6,19 @@ const forecast = require("./utils/forecast");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
-app.options("*", cors());
+// app.use(cors());
+// app.options("*", cors());
 const port = process.env.PORT || 3001;
-console.log("port", port);
+const corsOptions = {
+  origin: "https://gabai-weather-app.herokuapp.com/",
+  optionsSuccessStatus: 200,
+};
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "client/build");
 app.use(express.static(publicDirectoryPath));
-// const viewsPath = path.join(__dirname, "../templates/views");
-// const partialsPath = path.join(__dirname, "../templates/partials");
 
-// // Setup handlebars engine and views location
-// app.set('view engine', 'hbs');
-// app.set('views', viewsPath);
-// hbs.registerPartials(partialsPath);
-
-// // Setup static directory to serve
-// app.use(express.static(publicDirectoryPath));
-
-// app.get('', (req, res) => {
-//     res.render('index', {
-//         title: 'Weather',
-//         name: 'Andrew Mead',
-//     });
-// });
-
-// app.get('/about', (req, res) => {
-//     res.render('about', {
-//         title: 'About Me',
-//         name: 'Andrew Mead',
-//     });
-// });
-
-// app.get('/help', (req, res) => {
-//     res.render('help', {
-//         helpText: 'This is some helpful text.',
-//         title: 'Help',
-//         name: 'Andrew Mead',
-//     });
-// });
-
-app.get("/api/weather", (req, res) => {
+app.get("/api/weather", cors(corsOptions), (req, res) => {
   if (!req.query.address) {
     return res.send({
       error: "You must provide an address!",
@@ -75,35 +46,6 @@ app.get("/api/weather", (req, res) => {
     }
   );
 });
-
-// app.get('/products', (req, res) => {
-//     if (!req.query.search) {
-//         return res.send({
-//             error: 'You must provide a search term',
-//         });
-//     }
-
-//     console.log(req.query.search);
-//     res.send({
-//         products: [],
-//     });
-// });
-
-// app.get('/help/*', (req, res) => {
-//     res.render('404', {
-//         title: '404',
-//         name: 'Andrew Mead',
-//         errorMessage: 'Help article not found.',
-//     });
-// });
-
-// app.get('*', (req, res) => {
-//     res.render('404', {
-//         title: '404',
-//         name: 'Andrew Mead',
-//         errorMessage: 'Page not found.',
-//     });
-// });
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
